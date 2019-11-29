277be18fd1da832ebb9c89d70f62b39140d0ca2a
Edyta2801 <edyta.szarowska@interia.pl>
2019-11-29 11:18:02 +0100

    destructure props ninjas as argument odf function

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
 
-const Ninjas = (props) => {
-
-    const { ninjas } = props;
+const Ninjas = ({ninjas}) => {
     const ninjaList = ninjas.map(ninja => {
         return (
             <div className="ninja" key={ninja.id}>
