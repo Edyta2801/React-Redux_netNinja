539203efd038330a81595eed6cc3110156ceb822
Edyta2801 <edyta.szarowska@interia.pl>
2019-11-29 12:51:42 +0100

    push new ninja to data

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,21 +3,30 @@ import Ninjas from './Ninjas';
 import AddNinja from './AddNinja';
 
 
-class App extends Component{
-  state={
-    ninjas:[
-      {name:"Ryu", age:"25" ,belt:"black", id:1},
-      {name:"Yoshi", age:"20" ,belt:"green", id:2},
-      {name:"Crystal", age:"30" ,belt:"pink", id:3}
+class App extends Component {
+  state = {
+    ninjas: [
+      { name: "Ryu", age: "25", belt: "black", id: 1 },
+      { name: "Yoshi", age: "20", belt: "green", id: 2 },
+      { name: "Crystal", age: "30", belt: "pink", id: 3 }
     ]
   }
-  render(){
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja];
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+
+
+  render() {
     return (
       <div className="App">
         <h1>My first React app!</h1>
         <h2>Welcome :)</h2>
-        <Ninjas ninjas={this.state.ninjas}/>
-        <AddNinja/>
+        <Ninjas ninjas={this.state.ninjas} />
+        <AddNinja addNinja={this.addNinja} />
       </div>
     )
   }
