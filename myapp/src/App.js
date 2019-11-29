2d749452468978249f2b249d58a06c7080b80a97
Edyta2801 <edyta.szarowska@interia.pl>
2019-11-29 10:20:18 +0100

    console.log  props

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -8,7 +8,7 @@ class App extends Component{
       <div className="App">
         <h1>My first React app!</h1>
         <h2>Welcome :)</h2>
-        <Ninjas/>
+        <Ninjas name="Ryu" age="25" belt="black"/>
       </div>
     )
   }
--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react';
 
 class Ninjas extends Component {
     render() {
+        console.log(this.props);
         return (
             <div className="ninja">
-                <div> Name:Ryu</div>
+                <div> Name:30</div>
                 <div> Age:30</div>
                 <div> Belt:Black</div>
             </div>
