32c43cc6dca7d00df89ce674352dc3ec3b907cd7
Edyta2801 <edyta.szarowska@interia.pl>
2019-12-01 00:46:00 +0100

    add Routes

--- myapp/src/components/Navbar.js
+++ myapp/src/components/Navbar.js
@@ -7,8 +7,8 @@ const Navbar = () => {
                 <a className="brand-logo">Poke'Times</a>
                 <ul className="right">
                     <li><a href="/">Home</a></li>
-                    <li><a href="/">About</a></li>
-                    <li><a href="/">Contact</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/contact">Contact</a></li>
                 </ul>
             </div>
         </nav>
