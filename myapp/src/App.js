cab107be36d8b11c39d290b04701fec44c046a78
Edyta2801 <edyta.szarowska@interia.pl>
2019-12-01 00:24:01 +0100

    add components toshow React Router

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,50 +1,13 @@
 import React, { Component } from 'react';
-import Ninjas from './Ninjas';
-import AddNinja from './AddNinja';
+
 
 
 class App extends Component {
-  state = {
-    ninjas: [
-      { name: "Ryu", age: "25", belt: "black", id: 1 },
-      { name: "Yoshi", age: "20", belt: "green", id: 2 },
-      { name: "Crystal", age: "30", belt: "pink", id: 3 }
-    ]
-  }
-  addNinja = (ninja) => {
-    ninja.id = Math.random();
-
-    /* copy array using spread operator and add new array */
-    let ninjas = [...this.state.ninjas, ninja];
-    this.setState({
-      ninjas: ninjas
-    })
-  }
-  deleteNinja = (id) => {
-    let ninjas = this.state.ninjas.filter(ninja => {
-      return ninja.id !== id
-      // if id =id is equal false
-    });
-    this.setState({
-      ninjas: ninjas
-    })
-  }
-  componentDidMount() {
-    console.log('component mounted')
-  }
-  componentDidUpdate(prevProp, prevState) {
-    console.log('component updated');
-    console.log(prevProp, prevState);
-
-  }
 
   render() {
     return (
       <div className="App">
-        <h1>My first React app!</h1>
-        <h2>Welcome :)</h2>
-        <Ninjas deleteNinja={this.deleteNinja} ninjas={this.state.ninjas} />
-        <AddNinja addNinja={this.addNinja} />
+
       </div>
     )
   }
---
+++ myapp/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='container'>
+            <h4 className='center'>Home</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea voluptates, dolore omnis vero doloremque id iusto quo possimus eveniet, sapiente ut labore animi exercitationem. Molestias doloremque ipsum vero quaerat obcaecati.</p>
+
+        </div>
+    )
+}
+export default Home;
