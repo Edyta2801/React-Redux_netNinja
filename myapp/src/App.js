fa43c837af82ca729035d65bcb8945869d8c61c3
Edyta2801 <edyta.szarowska@interia.pl>
2019-12-01 00:32:30 +0100

    npm install react-router-dom

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Navbar from './components/Navbar';
 
 
 
@@ -7,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Navbar />
       </div>
     )
   }
