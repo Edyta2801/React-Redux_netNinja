e667b4202df1b2a95ed5c026856055bee74eee9e
Edyta2801 <edyta.szarowska@interia.pl>
2019-12-01 23:11:34 +0100

    modify

--- myapp/src/HOC/Rainbow.js
+++ myapp/src/HOC/Rainbow.js
@@ -8,8 +8,7 @@ const Rainbow = (WrappedComponent) => {
     return (props) => {
         return (
             <div className={className}>
-                <WrappedComponent {...props}/>
-
+                <WrappedComponent {...props} />
             </div>
         )
     }
