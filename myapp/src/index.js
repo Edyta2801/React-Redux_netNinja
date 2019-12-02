b9dd828d91438a55d1e3a87a914b3f2f10ee69b0
Edyta2801 <edyta.szarowska@interia.pl>
2019-12-02 22:56:26 +0100

    create reducer

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -4,8 +4,9 @@ import './index.css';
 import App from './App';
 import {createStore} from 'redux';
 import {Provider}from 'react-redux';
+import rootReducer from '../src/reducers/rootReducer';
 
-const store=createStore();
+const store=createStore(rootReducer);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
