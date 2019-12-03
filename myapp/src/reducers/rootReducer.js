9842cc9e3f8c6560b9c2af983dc4525aed9e9ee6
Edyta2801 <edyta.szarowska@interia.pl>
2019-12-03 13:37:53 +0100

    change data

--- myapp/src/reducers/rootReducer.js
+++ myapp/src/reducers/rootReducer.js
@@ -1,13 +1,25 @@
 const initState = {
     posts: [
-        { id: 1, title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit", body: "quia et suscipit↵suscipit recusandae consequuntur …strum rerum est autem sunt rem eveniet architecto" },
-        { id: 2, title: "qui est esse", body: "est rerum tempore vitae↵sequi sint nihil reprehend…aperiam non debitis possimus qui neque nisi nulla" },
-        { id: 3, title: "ea molestias quasi exercitationem repellat qui ipsa sit aut", body: "et iusto sed quo iure↵voluptatem occaecati omnis e…↵molestiae porro eius odio et labore et velit aut" },
+        { id: '1', title: 'Squirtle Laid an Egg', body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur voluptate laborum perferendis, enim repellendus ipsam sunt autem at odit dolorum, voluptatum suscipit iste harum cum magni itaque animi laudantium fugiat' },
+        { id: '2', title: 'Charmander Laid an Egg', body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur voluptate laborum perferendis, enim repellendus ipsam sunt autem at odit dolorum, voluptatum suscipit iste harum cum magni itaque animi laudantium fugiat' },
+        { id: '3', title: 'a Helix Fossil was Found', body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur voluptate laborum perferendis, enim repellendus ipsam sunt autem at odit dolorum, voluptatum suscipit iste harum cum magni itaque animi laudantium fugiat' }
     ]
 }
 
 
+
 const rootReducer = (state = initState, action) => {
+    console.log(action);
+    if(action.type === 'DELETE_POST'){
+     let newPosts = state.posts.filter(post => {
+       return post.id !== action.id
+     });
+     return {
+       ...state,
+       posts: newPosts
+     }
+    }
     return state;
-}
-export default rootReducer;
+  }
+
+  export default rootReducer
